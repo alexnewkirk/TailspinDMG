5994e6f90cb8004ba0a075a4711ad7fcd894347d
anewkirk <alex.newkirk0@gmail.com>
2016-10-10 12:50:16 -0600

    Finished header-read code in core.RomFile

--- src/com/echodrop/gameboy/core/RomFile.java
+++ src/com/echodrop/gameboy/core/RomFile.java
@@ -1,13 +1,16 @@
 package com.echodrop.gameboy.core;
 
+import com.echodrop.gameboy.util.NumberUtils;
+import com.echodrop.gameboy.util.StringUtils;
+
 public class RomFile {
 	
 	public String title;
 	public String mfgCode;
 	public byte cartridgeType;
+	public long romSize;
 	public byte ramSize;
-	public byte romSize;
-	public boolean destCodeJp;
+	public byte destCode;
 	public byte headerChecksum;
 	public char cartridgeChecksum;
 	
@@ -15,25 +18,37 @@ public class RomFile {
 	public byte[] ram;
 	
 	public RomFile(byte[] rom) {
-		byte[] titleBytes = new byte[16];
-		for(int i = 0x134; i < 0x143; i++) {
-			titleBytes[i-0x134] = rom[i];
+		this.romData = rom;
+		this.title = new String(readSection(0x134, 0x143));
+		this.mfgCode = new String(readSection(0x13f, 0x142));
+		this.cartridgeType = romData[0x147];
+		this.romSize = 32000 << romData[0x148];
+		this.ramSize = romData[0x149];
+		this.destCode = romData[0x14A];;
+		this.headerChecksum = romData[0x14D];
+		
+		byte[] globalChecksumBytes = readSection(0x14E, 0x150);
+		this.cartridgeChecksum = NumberUtils.bytesToWord(globalChecksumBytes[0], globalChecksumBytes[1]);
+	}
+	
+	public byte[] readSection(int start, int end) {
+		if(romData != null) {
+			byte[] sectionBytes = new byte[end - start];
+			for(int i = start; i < end; i++) {
+				sectionBytes[i-start] = romData[i];
+			}
+			return sectionBytes;
+		} else {
+			throw new NullPointerException("ROM data is null, could not read section");
 		}
-		this.title = new String(titleBytes);
-		
-		byte[] mfgCodeBytes = new byte[3];
-		for(int i = 0x13f; i < 0x142; i++) {
-			mfgCodeBytes[i-0x13f] = rom[i];
-		}
-		this.mfgCode = new String(mfgCodeBytes);
-		
-		
 	}
 	
 	@Override
 	public String toString() {
 		return "ROM HEADER INFO:\n" + title + "\nMFG CODE: " + mfgCode +
 				"\nCARTRIDGE TYPE: " + cartridgeType + "\nROM SIZE: " + 
-				romSize + "\nRAM SIZE: " + ramSize + "\nDESTINATION CODE: ";
+				romSize + "\nRAM SIZE: " + ramSize + "\nDESTINATION CODE: " +
+				destCode + "\nHEADER CHECKSUM: " + StringUtils.byteToReadableHex(headerChecksum) + 
+				"\nCARTRIDGE CHECKSUM: " + StringUtils.charToReadableHex(cartridgeChecksum);
 	}
 }
