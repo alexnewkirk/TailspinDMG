458f7dce319ea0193130768276868a5440a618f6
anewkirk <alex.newkirk0@gmail.com>
2016-10-15 15:57:33 -0600

    started work on Processing UI. Emulator is running *far* too slow to be playable at the moment, GPU needs refactoring

---
+++ src/com/echodrop/gameboy/ui/processing/Launcher.java
@@ -0,0 +1,87 @@
+package com.echodrop.gameboy.ui.processing;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import com.echodrop.gameboy.debugger.TailspinDebugger;
+import com.echodrop.gameboy.exceptions.MapperNotImplementedException;
+import com.echodrop.gameboy.graphics.GPU;
+import com.echodrop.gameboy.interfaces.IGraphicsObserver;
+import com.echodrop.gameboy.logging.SimpleConsoleLogger;
+import com.echodrop.gameboy.util.FileUtils;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Launcher extends PApplet implements IGraphicsObserver {
+	
+	private GPU gpu;
+	private byte[][] screen;
+	private TailspinDebugger tdb;
+	private PImage buffer;
+	
+	public Launcher() throws IOException, MapperNotImplementedException {
+		this.tdb = new TailspinDebugger();
+		this.gpu = tdb.getSystem().getGpu();
+		gpu.registerObserver(this);
+		tdb.getSystem().initLogging(Level.OFF, new SimpleConsoleLogger());
+		
+		this.buffer = new PImage(160, 144);
+		screen = gpu.getFrameBuffer();
+		
+		byte[] drmario = FileUtils.readBytes("roms/drmario.gb");
+		byte[] bootstrap = FileUtils.readBytes("bios.gb");
+		tdb.getSystem().getMem().loadBootstrap(bootstrap);
+		tdb.getSystem().getMem().loadRom(drmario);
+		
+		thread("startEmu");
+	}
+
+	public static void main(String[] args) {
+		PApplet.main("com.echodrop.gameboy.ui.processing.Launcher");
+	}
+	
+	@Override
+    public void settings(){
+		size(160, 144);
+    }
+	
+	@Override
+    public void draw(){
+		image(buffer, 0, 0);
+    }
+
+	@Override
+	public void updateDisplay() {
+		this.screen = gpu.getFrameBuffer();
+		buffer.loadPixels();
+		if(screen != null) {
+			for(int i = 0; i < 160; i++) {
+				for(int j = 0; j < 144; j++) {
+					int c = 0;
+					switch(screen[i][j]) {
+					case 0:
+						c = 255;
+						break;
+					case 1:
+						c = (255/3) * 2;
+						break;
+					case 2: 
+						c = 155/3;
+						break;
+					case 3:
+						c = 0;
+						break;
+					}
+					buffer.pixels[j*160 + i] = color(c);
+				}
+			}
+			buffer.updatePixels();
+		}
+	}
+	
+	public void startEmu() {
+		tdb.getSystem().getProcessor().beginDispatch();
+	}
+
+}
